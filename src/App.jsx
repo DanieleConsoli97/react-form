0e193ff16724368695af88954d28ad0456572cf4
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-02-25 19:26:33 +0100

    end

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,42 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([])
+  const [addPost, setAddPosts] = useState("")
+  const handleAddPosts=(e)=>{
+    setAddPosts(e.target.value)
+    console.log(e)
+    console.log(e.target.value)
 
+  }
+  const handleSub = (e)=>{
+    e.preventDefault()
+    setPosts(()=>[...posts,addPost])
+  }
   return (
     <>
+      <div>
+        <form onSubmit={handleSub}>
+          <input type="text" value={addPost} onChange={handleAddPosts} />
+          <button>add Post</button>
+        </form>
       
+      </div>
+      {
+        posts.length > 0 ?(
+          <ul>
+            {
+              posts.map((post,i)=>{
+                return(
+                <li key={i}>{post}</li>
+              )
+              })
+            }
+          </ul>
+        ):(
+          <h2>Nessun Post presente</h2>
+        )
+      }
     </>
   )
 }
